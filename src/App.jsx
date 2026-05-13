98ac0005c5f7e240e6e1599bd2a5355d532d739d
Celia <joyihirwecelia@gmail.com>
2026-05-14 01:07:03 +0200

    Plateform

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,27 @@ import LandingPage from './pages/LandingPage'
 import CheckEmailPage from './pages/auth/CheckEmailPage'
 import ResetPasswordPage from './pages/auth/ResetPasswordPage'
 import LinkSentPage from './pages/auth/LinkSentPage'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-export default function App(){
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LandingPage/>}/>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/check-email" element={<CheckEmailPage />} />
-                <Route path="/reset-password" element={<ResetPasswordPage />} />
-                <Route path="/link-sent" element={<LinkSentPage/>}/>
-                <Route path="/dashboard" element={<DashboardLayout/>}>
-                    <Route index element={<div>Dashboard</div>}/>
-                    
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+import DashboardLayout from './components/layout/DashboardLayout'
+import OverviewPage from './pages/dashboard/OverviewPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/check-email" element={<CheckEmailPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/link-sent" element={<LinkSentPage />} />
+
+        
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<OverviewPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ src/pages/dashboard/OverviewPage.jsx
@@ -0,0 +1,10 @@
+function OverviewPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-gray-900">Overview</h1>
+      <p className="text-gray-400 mt-1">Welcome to your dashboard</p>
+    </div>
+  )
+}
+
+export default OverviewPage
